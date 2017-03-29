08a8d32e95eedd7c44bf2f2948fa670d362d0a38
Tae Kim <taekimsf@gmail.com>
2017-03-29 16:40:58 -0700

    added neccessary imports to Voting_spec.jsx and wrote initial test for button render

---
+++ test/components/Voting_spec.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+	renderIntoDocument
+} from 'react-addons-test-utils';
+import Voting from '../../src/components/Voting';
+
+describe('Voting', () => {
+
+	it('render a pair of buttons', () => {
+		const component = renderIntoDocument(
+			<Voting pair={["Trainspotting", "28 Days Later"]} />
+		);
+	});
+
+});
+
+
+
+
+
+
+
