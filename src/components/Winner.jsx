43c9bc7ffd33de2615baf5659625371c4086e370
Tae Kim <taekimsf@gmail.com>
2017-03-31 23:40:09 -0700

    added test for button disable state in Voting_spec

--- src/components/Winner.jsx
+++ src/components/Winner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default React.createClass({
 	render: function() {
-		return <div className:"winner">
+		return <div className="winner">
 			winner is {this.props.winner}
 		</div>;
 	}
--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {
 	renderIntoDocument,
 	scryRenderedDOMComponentsWithTag,
-	simulate
+	Simulate
 } from 'react-addons-test-utils';
 import Voting from '../../src/components/Voting';
 import {expect} from 'chai'
@@ -21,6 +21,24 @@ describe('Voting', () => {
 		expect(buttons[1].textContent).to.equal('28 Days Later');
 	});
 
+	it('invokes callback when button is clicked', () => {
+		let votedWith;
+		const vote = entry => votedWith = entry;
+
+		const component = renderIntoDocument
+	});
+
+	it('disables buttons when user has voted', () => {
+	  const component = renderIntoDocument(
+	    <Voting pair={["Trainspotting", "28 Days Later"]}
+	            hasVoted="Trainspotting" />
+	  );
+	  const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+	  expect(buttons.length).to.equal(2);
+	  expect(buttons[0].hasAttribute('disabled')).to.equal(true);
+	  expect(buttons[1].hasAttribute('disabled')).to.equal(true);
+	});
 });
 
 
