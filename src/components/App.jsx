c6a24007ee1ac8c219fdf5ce89e477783233c541
Tae Kim <taekimsf@gmail.com>
2017-04-08 23:23:14 -0700

    fixed syntax errors in Results and App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {List, Map} from 'immutable';
 
 const pair = List.of('Trainspotting', '28 Days Later');
-const tally = Map({'Trainspotting: 5', '28 Days Later: 4'});
+const tally = Map({'Trainspotting': 5, '28 Days Later': 4});
 
 export default React.createClass({
 	render: function() {
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -15,7 +15,7 @@ export default React.createClass({
 	render: function() {
 		return <div className="results">
 			{this.getPair().map(entry =>
-				<div key=entry className="entry">
+				<div key={entry} className="entry">
 					<h1>{entry}</h1>
 					<div className="voteCount">
 						{this.getVotes(entry)}
