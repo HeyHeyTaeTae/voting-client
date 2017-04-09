ef5765bdf5a64410a7d65c508d11f90c2712992a
Tae Kim <taekimsf@gmail.com>
2017-04-08 22:37:28 -0700

    added necessary imports in Results_spec

---
+++ test/components/Results_spec.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {
+	renderIntoDocument,
+	scryRenderedDOMComponentsWithClass
+} from 'react-addons-test-utils';
+import {List, Map} from 'immutable';
+import Results from '../../src/components/Results';
+import {expect} from 'chai';
+
